fb47a807b98bdf08cee3d4574193fa70ef4f703b
zainab <zainabkashif253@gmail.com>
2022-10-14 16:54:18 +0500

    Initial commit

---
+++ public/app.js
@@ -0,0 +1,14 @@
+function btn(){
+    alert("YOUR RESPONSE HAS BEEN SUBMITTED");
+    alert("We are looking forward to escalate our services.Kindly give us feedback on our facebook account.Your reviews will really help us.")
+}
+
+function ser1(){
+    alert("If you want to avail this service then a total of 35$ would be charged.The price is fixed");
+}
+function ser2(){
+    alert("If you want to avail this service then a total of 30$ would be charged.The price is fixed");
+}
+function ser3(){
+    alert("If you want to avail this service then a total of 40$ would be charged.The price is fixed");
+}
